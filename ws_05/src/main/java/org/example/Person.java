c78294fb3642e5e7ac9bd276af5b25793b0d5440
yoonee123 <78791337+yoonee123@users.noreply.github.com>
2024-10-10 12:44:53 +0900

    Update Person.java

--- ws_05/src/main/java/org/example/Person.java
+++ ws_05/src/main/java/org/example/Person.java
@@ -4,6 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+@Service
+@Repository
+@Controller
+
 @Component
 public class Person {
     String name;
