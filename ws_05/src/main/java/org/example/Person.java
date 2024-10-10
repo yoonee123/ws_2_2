a891d35d8c1737868f5510b4e4bb93645a3d596b
yoonee123 <78791337+yoonee123@users.noreply.github.com>
2024-10-10 12:45:24 +0900

    Update Person.java

--- ws_05/src/main/java/org/example/Person.java
+++ ws_05/src/main/java/org/example/Person.java
@@ -3,6 +3,9 @@ package org.example;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 @Service
 @Repository
